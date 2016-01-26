cb707fe21223683b60277c1480e542ff6366a9a8
magro <magro@linux-2dzr.site>
2016-01-26 12:27:26 +0100

    hw10

--- diffusion.cxx
+++ diffusion.cxx
@@ -17,7 +17,7 @@ void step(double* const u1,  double* const u0,  const double dt,
 int main(){
 
   const double tEnd = 5 ;
-  const double D = 1;
+  const double D = 0.3;
 
   const int N  = 200;
   const double xmin = -20;
@@ -44,6 +44,12 @@ int main(){
   for(int i=1; i<=Na; i++)
   {
    for(int j=0; j<Nk; j++){
+     
+     step(u1, u0, dt, dx, D, N);
+     h = u0;
+     u0 = u1;
+     u1 = h;
+     t += dt;
 
 
    }
@@ -59,10 +65,16 @@ int main(){
   return 0;
 }
 //-----------------------------------------------
-void step(double* const f1, double* const f0,
+void step(double* const u1, double* const u0,
           const double dt, const double dx,
           const double D, const int N)
 {
+  u1[0] = (dt*((D/(dx*dx))*(u0[1]-2*u0[0]+u0[N-1])))+u0[0];
+  u1[N-1] = (dt*((D/(dx*dx))*(u0[0]-2*u0[N-1]+u0[N-2])))+u0[N-1];
+  
+  for(int i = 1; i<=N-1; i++)
+    u1[i] = (dt*((D/(dx*dx))*(u0[i+1]-2*u0[i]+u0[i-1])))+u0[i];
+  
 
 }
 //-----------------------------------------------
